ba7d14a43993d1b9347fae55b71093adee17404f
Bryan Cole <bryan.cole@hpe.com>
2017-08-22 00:39:12 -0600

    initial

---
+++ src/main/java/net/hpe/SeleniumTest.java
@@ -0,0 +1,149 @@
+package net.hpe;
+
+import static org.junit.Assert.*;
+
+import com.google.common.base.Verify;
+import com.hp.lft.report.Reporter;
+import com.hp.lft.report.Status;
+import com.sun.jndi.toolkit.url.Uri;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.pagefactory.ByChained;
+import com.hpe.leanft.selenium.By;
+import com.hpe.leanft.selenium.ByEach;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
+
+import com.hp.lft.sdk.*;
+import com.hp.lft.sdk.web.*;
+import com.hp.lft.verifications.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SeleniumTest  {
+
+    public SeleniumTest() {
+    //Change this constructor to private if you supply your own public constructor
+    }
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // initialize the SDK and report only once per process
+        // this needs to be done since you are using a different framework than LFT.
+        try{
+
+            ModifiableSDKConfiguration config = new ModifiableSDKConfiguration();
+            config.setServerAddress(new URI("ws://localhost:5095"));
+            SDK.init(config);
+
+            Reporter.init();
+        }
+        catch(Exception e){
+            System.out.println("ERROR OCCURRED: \n"+e.toString());
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //Generate the report and cleanup the SDK usage.
+        Reporter.generateReport();
+        SDK.cleanup();
+    }
+
+    @Test
+    public void test() throws Exception {
+        //Reporter.init();
+
+        // Location of where your chromedriver is locate.
+        // If you don't use the setPropery, then you will need to have chromedriver in your system path
+        System.setProperty("webdriver.chrome.driver", "/opt/selenium/2.29/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        WebDriverWait w = new WebDriverWait(driver,20);
+        w.ignoring(NoSuchElementException.class);
+        org.openqa.selenium.WebElement P = null;
+
+        try {
+            driver.get("http://www.advantageonlineshopping.com");
+
+            // This line is using the HPE extension of Selenium through the new attribute 'visibleText'
+            w.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("TABLETS")));  //this is one way to sync on objects in Selenium
+            //P=w.until(ExpectedConditions.elementToBeClickable(By.visibleText("TABLETS")));  //this is one way to sync on objects in Selenium
+            driver.findElement(By.visibleText("TABLETS")).click();
+
+            // This line is using normal Selenium attributes
+            w.until(ExpectedConditions.visibilityOfElementLocated(By.id("accordionPrice")));  //this is one way to sync on objects in Selenium
+            driver.findElement(By.id("accordionPrice")).click();
+
+            String priceAccordian = "/html/body/div[3]/section/article/div[3]/div/div/div[2]/ul/li[1]/p[2]/a";
+            w.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(priceAccordian)));
+            String price = driver.findElement(By.xpath(priceAccordian)).getText();
+            com.hp.lft.verifications.Verify.areEqual("$1,009.00", price);
+
+            // The following will attach LeanFT to the browser opened by Selenium and work using LeanFT libraries
+            Reporter.reportEvent("Attach to Browser","Current URL: "+driver.getCurrentUrl().toString(),Status.Passed);
+            Browser browser = BrowserFactory.attach(new BrowserDescription.Builder().url(driver.getCurrentUrl().toString()).build());
+
+            browser.describe(Image.class, new ImageDescription.Builder()
+                    .className("imgProduct")
+                    .src(new RegExpProperty(".*image_id=3100"))
+                    .tagName("IMG")
+                    .type(com.hp.lft.sdk.web.ImageType.NORMAL).build()).highlight();
+
+        }
+        catch (Exception e){
+            Reporter.reportEvent("Test Fail", "Error in script execution<br>Check the error thrown", Status.Failed, e);
+        }
+        finally {
+            driver.quit();
+        }
+
+    }
+/*
+    @Test
+    public void simple (){
+        System.setProperty("webdriver.chrome.driver", "/opt/selenium/2.29/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("http://www.google.com");
+            ModifiableSDKConfiguration config = new ModifiableSDKConfiguration();
+            config.setServerAddress(new URI("ws://dockerserver:5096"));
+            SDK.init(config);
+
+            Thread.sleep(3000);
+            // The following will attach LeanFT to the browser opened by Selenium and work using LeanFT libraries
+            System.out.println("Current title: "+driver.getTitle().toString());
+            System.out.println();
+            //Browser browser = BrowserFactory.attach(new BrowserDescription.Builder().title(driver.getTitle().toString()).build());
+            Browser browser = BrowserFactory.attach(new BrowserDescription.Builder().type(BrowserType.CHROME).build());
+            browser.highlight();
+        }
+        catch (Exception e){
+            System.out.println("Flynn Error: \n"+e.toString());
+        }
+        finally {
+            driver.quit();
+            SDK.cleanup();
+        }
+
+    }
+    */
+
+}
